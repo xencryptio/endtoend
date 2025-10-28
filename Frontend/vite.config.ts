79fba64d30efa2d483b34d8a90a7d12588eb6398
Nipun Negi <100460166+nipunnegi2@users.noreply.github.com>
2025-10-28 11:12:25 +0530

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,16 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: '0.0.0.0',
     port: 8080,
+    allowedHosts: ['all'], // allow connections from any host
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
