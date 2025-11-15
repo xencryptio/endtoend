1f889e28e84e7cb4d065e28e51c029ff50b2031e
tejasaibottu <150500883+tejasaibottu@users.noreply.github.com>
2025-11-15 10:16:52 +0530

    resolving db creation problem

--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -5,13 +5,11 @@
 CREATE DATABASE scandb;
 CREATE DATABASE repo_scanner_db;
 CREATE DATABASE system_scanner_db;
-CREATE DATABASE scanuser;
 
 -- Grant all privileges to scanuser on each database
 GRANT ALL PRIVILEGES ON DATABASE scandb TO scanuser;
 GRANT ALL PRIVILEGES ON DATABASE repo_scanner_db TO scanuser;
 GRANT ALL PRIVILEGES ON DATABASE system_scanner_db TO scanuser;
-GRANT ALL PRIVILEGES ON DATABASE scanuser TO scanuser;
 
 -- Connect to each database to enable extensions and set permissions.
 -- This is necessary because some commands are database-specific.
@@ -25,9 +23,5 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 GRANT ALL ON SCHEMA public TO scanuser;
 
 \c system_scanner_db;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-GRANT ALL ON SCHEMA public TO scanuser;
-
-\c scanuser;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 GRANT ALL ON SCHEMA public TO scanuser;
